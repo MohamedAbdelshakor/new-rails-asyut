863d0158469218d03d8497ab61804ca409db75a2
mohammedd2002 <168116874+mohammedd2002@users.noreply.github.com>
2026-06-13 12:55:15 +0300

    Implement CRUD actions for posts and update routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,59 @@
 class PostsController < ApplicationController
-  # your code goes here
+
+
+  # GET /posts
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/:id
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(
+      title: params[:post][:title],
+      content: params[:post][:content]
+    )
+
+    if @post.save
+      redirect_to @post, notice: "Post Created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # GET /posts/:id/edit
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  # PATCH /posts/:id
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(
+      title: params[:post][:title],
+      content: params[:post][:content]
+    )
+      redirect_to @post, notice: "Post Updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /posts/:id
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path, notice: "Post Deleted!"
+  end
 end
